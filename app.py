039c617fb9ccc89beccf70a368119722e7c68c82
kushal1997 <kushalrao103@gmail.com>
2025-07-05 00:49:26 +0530

    Take 2 - changes to test pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Its Noizy aka Kushal - Take1"
+    return "Hello, Its Noizy aka Kushal - Take2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
