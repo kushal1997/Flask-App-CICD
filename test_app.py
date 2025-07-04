9b9f6283949dd130d8b5b46786d4e39239aba537
U KUSHAL RAO <kushalrao103@gmail.com>
2025-07-05 00:06:35 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, Jenkins World!" in response.data
+    assert b"Hello, Its Noizy aka Kushal" in response.data
