5594afc41331ef52e72bf7eb9f8f4654b6976a10
U KUSHAL RAO <kushalrao103@gmail.com>
2025-07-05 03:06:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Its Noizy aka Kushal - Take4 for failed mail"
+    return "Hello, Its Noizy aka Kushal - Take5 With all Ok"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
