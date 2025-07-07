df15abd06e4d386a1fe448d89e35fe2629a46c90
kushal1997 <kushalrao103@gmail.com>
2025-07-07 13:25:53 +0530

    Cleaning base files for final setup

--- test_app.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-def test_home():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
-    assert b"Hello, Its Noizy aka Kushal - Take5 With all Ok" in response.data
