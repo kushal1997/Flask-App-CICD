7e1f5a8f5a2efa7b9135f081b3fc36b1a564c461
U KUSHAL RAO <kushalrao103@gmail.com>
2025-07-07 11:11:20 +0530

    Take 2 - testing

--- 2_GithubActionPipeline/app.py
+++ 2_GithubActionPipeline/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Its Noizy aka Kushal - Take1 for Gihub Actions"
+    return "Hello, Its Noizy aka Kushal - Take2 for Gihub Actions - staging phase"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
