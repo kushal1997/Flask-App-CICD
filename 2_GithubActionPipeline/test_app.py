f45330df831add5f99b547b4dbcb03f8dfca2ff2
kushal1997 <kushalrao103@gmail.com>
2025-07-07 11:36:14 +0530

    Take3 - testing with tagiing

--- 2_GithubActionPipeline/test_app.py
+++ 2_GithubActionPipeline/test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert b"Hello, Its Noizy aka Kushal - Take2 for Gihub Actions - staging phase" in response.data
+    assert b"Hello, Its Noizy aka Kushal - Take3 for Gihub Actions - taging" in response.data
